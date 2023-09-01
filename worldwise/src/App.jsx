10219affa490eccda9e7638da6db166ef88d6a82
iAmMuadh <adeyemimuaz1@gmail.com>
2023-09-01 12:23:22 +0100

    building the pages

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -3,6 +3,7 @@ import Homepage from './pages/Homepage'
 import Pricing from './pages/Pricing'
 import Product from './pages/Product'
 import PageNotFound from './pages/PageNotFound'
+import Login from './pages/Login'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path='/' element={<Homepage />} />
         <Route path='/pricing' element={<Pricing />} />
         <Route path='/product' element={<Product />} />
+        <Route path='/login' element={<Login />} />
         <Route path='*' element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
